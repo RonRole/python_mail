d779025a3872a72bca79dc1d4aeef870b9dff6f5
RonRole <ronrolenewadress@gmail.com>
2022-01-31 20:08:12 +0900

    ループ内でMailServerインスタンスを無駄に生成していたので、ループ外に移動

--- python_mail.py
+++ python_mail.py
@@ -20,11 +20,11 @@ mail_account_info = app.mailitems.MailAccountInfo(
     password   = MAIL_PASSWORD
 )
 
+mailserver = app.mailitems.MailServer(mail_account_info)
+
 with open(CSV_FILE_NAME, encoding='utf-8') as f:
     csv_lines = f.readlines()
     mailpartslist = app.mailitems.MailParts.load_csv_lines(csv_lines)
-
     for mailparts in mailpartslist:
-        mailserver = app.mailitems.MailServer(mail_account_info)
         mailserver.send(mailparts)
 
