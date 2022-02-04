f0c72eda890f070aab41b2ed2b4eccfe6cb5da0d
RonRole <ronrolenewadress@gmail.com>
2022-02-04 13:59:07 +0900

    とりあえず簡素なwebサーバー作成

--- python_mail.py
+++ python_mail.py
@@ -1,24 +1,9 @@
 import sys
 import json
-import app
+import mailsettings
 
-settings_json = open('settings.json', 'r')
-settings = json.load(settings_json)
-
-SMTP_SERVER = settings['smtpserver']
-SMTP_PORT = settings['smtpport']
-MAIL_ACCOUNT = settings['mail_account']
-MAIL_PASSWORD = settings['mail_password']
-
-mail_account_info = app.mailserver.MailAccountInfo(
-    smtpserver = SMTP_SERVER, 
-    smtpport   = SMTP_PORT, 
-    account    = MAIL_ACCOUNT, 
-    password   = MAIL_PASSWORD
-)
-
-mailserver = app.mailserver.MailServer(mail_account_info)
-mailpartsloader = app.mailpartsloader.MailPartsLoaderFactory.create(app.mailpartsloader.LoadType.CSV, **dict(
+mailserver = mailsettings.get_mailserver()
+mailpartsloader = app.mailpartsloader.MailPartsLoaderFactory.create(app.mailpartsloader.LoadType.CSV_FILE, **dict(
     mailfrom_idx = 0,
     mailto_idx = 1,
     subject_idx = 2,
