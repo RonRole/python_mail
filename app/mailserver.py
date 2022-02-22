0994da2d18200232fe0696faebb38d212650c30c
RonRole <ronrolenewadress@gmail.com>
2022-02-22 16:45:27 +0900

    python_mailで実行時間を出すように mailserverで、メール送信ごとにsmtpでlogin・quitしていた部分をenterとexitで実施するように修正(実行時間削減のため)

--- app/mailserver.py
+++ app/mailserver.py
@@ -11,13 +11,23 @@ class MailAccountInfo:
 class MailServer:
     def __init__(self, mail_account_info):
         self.__mail_account_info = mail_account_info
+        self.__smtpobj = smtplib.SMTP_SSL(
+            self.__mail_account_info.smtpserver, 
+            self.__mail_account_info.smtpport
+        )
+
+    def __enter__(self):
+        self.__smtpobj.login(
+            self.__mail_account_info.account, 
+            self.__mail_account_info.password
+        )
+
+    def __exit__(self, ex_type, ex_value, trace):
+        self.__smtpobj.quit()
 
     def send(self, mailparts):
         msg = email.mime.text.MIMEText(mailparts.contents)
         msg['Subject'] = mailparts.subject
         msg['From'] = mailparts.mail_from
         msg['To'] = mailparts.mail_to
-        smtpobj = smtplib.SMTP_SSL(self.__mail_account_info.smtpserver, self.__mail_account_info.smtpport)
-        smtpobj.login(self.__mail_account_info.account, self.__mail_account_info.password)
-        smtpobj.send_message(msg)
-        smtpobj.quit()
+        self.__smtpobj.send_message(msg)
--- python_mail.py
+++ python_mail.py
@@ -4,19 +4,45 @@ import json
 import re
 import settings
 import app
+import time
+
+def calc_func_time(func_name):
+    def decorator(func):
+        def target_func(*args, **kwargs):
+            start = time.perf_counter()
+            result = func(*args, **kwargs)
+            end = time.perf_counter()
+            print(f"{func_name} took {end - start} sec")
+            return result
+        return target_func
+    return decorator
 
 args = sys.argv
 LOAD_FILE_NAME = args[1]
 
 FULLPATH_SETTINGS_JSON = f'{os.path.dirname(os.path.abspath(__file__))}/settings.json'
 
-mailserver = settings.SettingsToMailServer.load(FULLPATH_SETTINGS_JSON)
-loadtypes = settings.SettingsToLoadTypes.load(FULLPATH_SETTINGS_JSON)
-(loadtype, load_settings) = loadtypes.matched_item(LOAD_FILE_NAME)
 
-mailpartsloader = app.MailPartsLoaderFactory.create(loadtype, **load_settings)
-mailpartslist = mailpartsloader.load(LOAD_FILE_NAME).group_by_address_and_subject()
+@calc_func_time('load_settings')
+def load_settings():
+    mailserver = settings.SettingsToMailServer.load(FULLPATH_SETTINGS_JSON)
+    loadtypes = settings.SettingsToLoadTypes.load(FULLPATH_SETTINGS_JSON)
+    (loadtype, load_settings) = loadtypes.matched_item(LOAD_FILE_NAME)
+    return (mailserver, loadtype, load_settings)
 
-for mailparts in mailpartslist:
-    mailserver.send(mailparts)
+@calc_func_time('load_mailpartslist')
+def load_mailpartslist(loadtype, **load_settings):
+    mailpartsloader = app.MailPartsLoaderFactory.create(loadtype, **load_settings)
+    mailpartslist = mailpartsloader.load(LOAD_FILE_NAME).group_by_address_and_subject()
+    return mailpartslist
+
+@calc_func_time('send_mails')
+def send_mails(mailserver, mailpartslist):
+    with mailserver:
+        for mailparts in mailpartslist:
+            mailserver.send(mailparts)
+
+(mailserver, loadtype, load_settings) = load_settings()
+mailpartslist = load_mailpartslist(loadtype, **load_settings)
+send_mails(mailserver, mailpartslist)
 
